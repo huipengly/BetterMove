2881ced2af974918c381eeb07f4b92b05df00a31
huipengly <huipengly@gmail.com>
2017-12-14 13:34:12 +0800

    修改float为double，删除相对移动函数

--- BetterMove/CppBetterMove/Elbow.cpp
+++ BetterMove/CppBetterMove/Elbow.cpp
@@ -1,16 +1,20 @@
 #include "Elbow.h"
 
-
-Elbow::Elbow() : pi(3.14159f)
+Elbow::Elbow() : pi(3.14159)
 {
+	//肘关节初始化参数
 	m1 = 0;
 	m2 = 0;
 	angle = 0;
 	rad = 0;
-	float kp = 1;
-	float ki = 7.5;
-	float kd = 0;
-	float t = 0.01;
+	maxout = 5;
+	minout = 0;
+
+	//肘关节PID初始化参数
+	double kp = 1;
+	double ki = 7.5;
+	double kd = 0;
+	double t = 0.01;
 	PID4Elbow.set(kp, ki, kd, t);
 }
 
@@ -19,119 +23,45 @@ Elbow::~Elbow()
 {
 }
 
-float Elbow::ShowRad()
-{
-	return rad;
-}
-
-float Elbow::ShowAngle()
-{
-	return angle;
-}
-
-float Elbow::ShowM1()
-{
-	return m1;
-}
-
-float Elbow::ShowM2()
-{
-	return m2;
-}
-
-void Elbow::GetAngle(float InputAngle)
+void Elbow::GetActualAngle(double InputAngle)
 {
 	angle = InputAngle;
 	rad = angle / 180 * pi;
 }
 
-void Elbow::SetMuscleRelative(float r1, float r2)
+void Elbow::SetMuscleAboslute(double a1, double a2)
 {
-	m1 += r1;
-	m2 += r2;
-
-	if (m1 > 5)
-	{
-		m1 = 5;
-	}
-	if (m1 < 0)
-	{
-		m1 = 0;
-	}
-
-	if (m2 > 5)
-	{
-		m2 = 5;
-	}
-	if (m2 < 0)
-	{
-		m2 = 0;
-	}
-
-	//if (m1 > 10)
-	//{
-	//	m1 = 10;
-	//}
-	//if (m1 < 0)
-	//{
-	//	m1 = 0;
-	//}
-
-	//if (m2 > 10)
-	//{
-	//	m2 = 10;
-	//}
-	//if (m2 < 0)
-	//{
-	//	m2 = 0;
-	//}
-}
-
-void Elbow::SetMuscleAboslute(float a1, float a2)
-{
+	/* 电压值转换，直接抄的计量程序 */
 	m1 = (5.0/6.0)*5.5 - a1;
 	m2 = a2;
 
-	if (m1 > 5)
+	if (m1 > maxout)
 	{
-		m1 = 5;
+		m1 = maxout;
 	}
-	if (m1 < 0)
+	if (m1 < minout)
 	{
-		m1 = 0;
-	}
-
-	if (m2 > 5)
-	{
-		m2 = 5;
-	}
-	if (m2 < 0)
-	{
-		m2 = 0;
+		m1 = minout;
 	}
 
-	//if (m1 > 10)
-	//{
-	//	m1 = 10;
-	//}
-	//if (m1 < 0)
-	//{
-	//	m1 = 0;
-	//}
-
-	//if (m2 > 10)
-	//{
-	//	m2 = 10;
-	//}
-	//if (m2 < 0)
-	//{
-	//	m2 = 0;
-	//}
+	if (m2 > maxout)
+	{
+		m2 = maxout;
+	}
+	if (m2 < minout)
+	{
+		m2 = minout;
+	}
 }
 
-void Elbow::SetTargetRad(float tar)
+void Elbow::SetTargetRad(double tarR)
 {
-	tarRad = tar;
+	tarRad = tarR;
+}
+
+void Elbow::SetTargetAngle(double tarA)
+{
+	tarRad = tarA / 180.0 * pi;
 }
 
 void Elbow::run()
@@ -139,3 +69,48 @@ void Elbow::run()
 	pressure = PID4Elbow.pid_control(tarRad, rad);
 	SetMuscleAboslute(pressure, pressure);
 }
+
+double Elbow::ShowRad()
+{
+	return rad;
+}
+
+double Elbow::ShowAngle()
+{
+	return angle;
+}
+
+double Elbow::ShowM1()
+{
+	return m1;
+}
+
+double Elbow::ShowM2()
+{
+	return m2;
+}
+
+/* 下面的代码是放在CycleUpdate函数所在的类里的 */
+/* CModule1改为CycleUpdate函数所在类的类名 *****/
+/* m_Outputs.m1Int这些改为所链接的变量名 *******/
+////void CModule1::ElbowUpdateInputs()
+////{
+////	//elbow.GetAngle(m_Inputs.Angle - 270);//不受力状态时，角度为270，减去，角度会增大//旧的肘
+////
+////	elbow.GetActualAngle(350 - m_Inputs.Angle);//不受力状态时，角度为350，减去，角度会减少//新的肘
+////
+////}
+////
+////void CModule1::ElbowUpdateOutputs()
+////{
+////	/* 输出给EL4004，用来控制比例压力阀。压力阀接收电压为0-10V */
+////	//电压转换为（0-10V）->（0-32767）
+////	m_Outputs.m1Int = static_cast<int>(elbow.ShowM1() / 10 * 32767);
+////	m_Outputs.m2Int = static_cast<int>(elbow.ShowM2() / 10 * 32767);
+////
+////	/* 输出个PLC用来观察的 */
+////	m_Outputs.rad = elbow.ShowRad();
+////	m_Outputs.tarrad = SetRad;
+////	m_Outputs.m1v = elbow.ShowM1();
+////	m_Outputs.m2v = elbow.ShowM2();
+////}
--- BetterMove/CppBetterMove/Elbow.h
+++ BetterMove/CppBetterMove/Elbow.h
@@ -8,24 +8,27 @@ class Elbow
 public:
 	Elbow();
 	~Elbow();
-	float ShowRad();//返回当前弧度值
-	float ShowAngle();
-	float ShowM1();
-	float ShowM2();
-	void GetAngle(float InputAngle);//获取角度值并转换为弧度值
-	//设置气动肌肉比例阀电压值，r1/a1代表单个肌肉一边，r2/a2代表2根肌肉一边
-	void SetMuscleRelative(float r1, float r2);//设置电压值相对变化量
-	void SetMuscleAboslute(float a1, float a2);//设置绝对电压值
-	void SetTargetRad(float tar);
+	void GetActualAngle(double InputAngle);//获取实际角度值并转换为弧度值
+	//设置气动肌肉比例阀电压值
+	//a1代表单个肌肉一边，a2代表2根肌肉一边
+	void SetMuscleAboslute(double a1, double a2);//设置绝对电压值
+	void SetTargetRad(double tarR);
+	void SetTargetAngle(double tarA);
 	void run();
+	double ShowRad();//返回当前弧度值
+	double ShowAngle();
+	double ShowM1();
+	double ShowM2();
 
 protected:
-	const float pi;	//数学常量PI
-	float angle;	//肘关节角度
-	float rad;		//肘关节弧度制角度
-	float m1, m2;	//气动肌肉比例阀电压值，m1代表单个肌肉一边，m2代表2根肌肉一边
-	float tarRad;	//肘关节目标角度
-	float pressure;	//气动肌肉压力值
+	const double pi;		//数学常量PI
+	double angle;		//肘关节角度
+	double rad;			//肘关节弧度制角度
+	double m1, m2;		//气动肌肉比例阀电压值，m1代表单个肌肉一边，m2代表2根肌肉一边
+	double tarRad;		//肘关节目标角度
+	double pressure;	//气动肌肉压力值
+	double maxout;		//气动阀最大输出电压
+	double minout;		//气动阀最小输出电压
 	PID_Position PID4Elbow;
 };
 
